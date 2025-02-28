069cd5844ba2a72a7bcf8e7626c834025b864e3b
Ruhini Mallawaarachchi <rsmmalla3@gmail.com>
2025-02-28 13:38:18 +0530

    Creating fileProcessor.js

---
+++ util/fileProcessor.js
@@ -0,0 +1,49 @@
+
+
+import * as DocumentPicker from "expo-document-picker";
+import * as FileSystem from "expo-file-system";
+import Pdf from "react-native-pdf";
+
+//Extracting text from a pdf file
+
+const extractTextFromPDF = async (pdfUri) => {
+    try{
+        const {uri} = await FileSystem.downloadAsync(
+            pdfUri, FileSystem.documentDirectory + "temp.pdf" 
+        );
+        return new Promise((resolve, reject) => {
+            const pdf = new Pdf({uri});
+            pdf.getTextContent().then((textContent) => {
+                const text = textContent.items.map((item) => item.str).join(" ");
+                resolve(text);
+            });
+
+
+        });
+    } catch (error){
+        console.error("Error extracting text from PDF:", error);
+        return null;
+      
+    }
+};
+
+
+// Extracting text from a TXT file
+
+const extractTextFromTXT = async (txtUri) => {
+    try {
+      const content = await FileSystem.readAsStringAsync(txtUri);
+      return content;
+    } catch (error) {
+      console.error("Error reading TXT file:", error);
+      return null;
+    }
+  };
+
+//Handles file picking and processing
+
+export const pickAndProcessFile = async () => {
+    const result = await DocumentPicker.getDocumentAsync({
+        type: "*/*" , //Allowing all file types
+    })
+}
